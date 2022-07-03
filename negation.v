f8685eaf448df677a2d3d7bea0af33f1b9cefd96
SharathRajM <108630410+SharathRajM@users.noreply.github.com>
2022-07-04 01:20:21 +0530

    Update negation.v

--- negation.v
+++ negation.v
@@ -1,30 +1,10 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/03/2022 11:52:19 PM
-// Design Name: 
-// Module Name: negation
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module negation;
     integer a,b,c;
     
     initial begin
-        assign a = 'b00;
+        assign a = 'b011001;
         assign b = ~a;
         $display("a = %0b",a);
         $display(" b = %0b ",b);
@@ -33,3 +13,8 @@ module negation;
     end
 
 endmodule
+
+
+
+
+
